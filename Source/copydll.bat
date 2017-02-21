cf318e08490b740a535973c04652e1afa1121ffe
zyl910 <zyl910@sina.com>
2017-02-21 17:51:48 +0800

    copydll.bat

---
+++ Source/copydll.bat
@@ -0,0 +1,4 @@
+copy /y ZylLib\bin\Release\Net20\*.* ..\Bin\Net20\
+copy /y ZylLib\bin\Release\Net35\*.* ..\Bin\Net35\
+copy /y ZylLib\bin\Release\Net40\*.* ..\Bin\Net40\
+copy /y ZylLib\bin\Release\Portable40\*.* ..\Bin\Portable40\
